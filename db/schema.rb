e61b99a9e15783a2c07d3dbf052bc976c5da70d7
meganeJPN <ooosamuuu1985@gmail.com>
2020-11-04 16:11:36 +0900

    データベースをpostgresqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_04_062455) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
